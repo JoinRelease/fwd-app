c85a1ba157def8e63c0547e6c06ba1afdb9b870a
Tai Dupree <tdupreetan@gmail.com>
2015-10-02 17:43:25 -0700

    init_forward

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+var React = require('react-native');
+var Home = require('./app/components/home');
+
+var {
+  AppRegistry
+} = React;
+
+AppRegistry.registerComponent('Forward', () => Home);
